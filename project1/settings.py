2d97618e5cd22740c0ff51d45c1c71ec3291ce4f
JeszuaHaNocri <lachtaraweronika@gmail.com>
2015-01-19 20:23:30 +0000

    Chapter 05 - completed

--- project1/settings.py
+++ project1/settings.py
@@ -13,6 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -23,6 +24,18 @@ SECRET_KEY = 'sw05!ezf*=50_nkb5hiv$ifk+mc^7@f*8zqd%w0v097_3o36nd'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIRS = [
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+]
+
+
+    #'C:Users/Wera/tangowithdjango/project1/',
+
 
 ALLOWED_HOSTS = []
 
@@ -81,4 +94,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATIC_URL = '/static/' # You may find this is already defined as such.
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
