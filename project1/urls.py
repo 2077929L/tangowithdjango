631702d0c1dd5d78121ec4930ef2bb5b5b3cfc39
JeszuaHaNocri <lachtaraweronika@gmail.com>
2015-02-27 13:53:43 +0000

    Chapter 12 - completed

--- project1/urls.py
+++ project1/urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,request, user):
+        return '/rango/'
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project1.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
-   
+    url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/change_password/$','django.contrib.auth.views.password_change',name="change_password"),
+    url(r'^accounts/password_change/done/$','django.contrib.auth.views.password_change_done',name='password_change_done'),
+    (r'^accounts/', include('registration.backends.simple.urls')),
 )
-
 
 if settings.DEBUG:
     urlpatterns += patterns(
