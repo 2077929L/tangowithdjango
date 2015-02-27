f9c7b5d3ef02d7689f308a6a1efccc688473cc6c
JeszuaHaNocri <lachtaraweronika@gmail.com>
2015-02-27 18:16:47 +0000

    Chapter 13 - almost completed

--- project1/urls.py
+++ project1/urls.py
@@ -12,8 +12,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
-    url(r'^accounts/change_password/$','django.contrib.auth.views.password_change',name="change_password"),
-    url(r'^accounts/password_change/done/$','django.contrib.auth.views.password_change_done',name='password_change_done'),
     (r'^accounts/', include('registration.backends.simple.urls')),
 )
 
